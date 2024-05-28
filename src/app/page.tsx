f3f5e2660db1d4e6488778b46bc68f55aeba0e14
rayhan889 <raydev1604@gmail.com>
2024-05-28 15:27:01 +0700

    force dynamic + vscode stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 const mokcUrls = [
   "https://utfs.io/f/f1abb778-a640-4679-84a1-dcc191deb198-6e676j.png",
   "https://utfs.io/f/50a3f2f7-aa80-4998-ab71-a6716b1d79d5-23tg.jpg",
