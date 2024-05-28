42d8e8b104788ce312110e52463ed327fa137058
rayhan889 <raydev1604@gmail.com>
2024-05-28 14:08:50 +0700

    skeleton ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import Link from "next/link";
 
+const mokcUrls = [
+  "https://utfs.io/f/f1abb778-a640-4679-84a1-dcc191deb198-6e676j.png",
+  "https://utfs.io/f/50a3f2f7-aa80-4998-ab71-a6716b1d79d5-23tg.jpg",
+];
+
+const mockImages = mokcUrls.map((url, index) => ({ id: index + 1, url }));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello Banggg!!!
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
